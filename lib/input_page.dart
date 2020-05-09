c6dc2872ad4230c88ca501686bf47c6898f44fd5
anniechakraborty <the.demented.genius29@gmail.com>
2020-05-10 00:52:41 +0530

    Current state of the app before refactoring the repetitive widget.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,9 +13,67 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row (
+                children: <Widget>[
+                  Expanded(
+                    child : Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1D1E33),     // as this is a default property of BoxDecoration widget, we cannot use it separately inside the Container widget.
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child : Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1D1E33),     // as this is a default property of BoxDecoration widget, we cannot use it separately inside the Container widget.
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  )
+                ],
+            ),
+          ),
+          Expanded(
+            child : Container(
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),     // as this is a default property of BoxDecoration widget, we cannot use it separately inside the Container widget.
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row (
+              children: <Widget>[
+                Expanded(
+                  child : Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),     // as this is a default property of BoxDecoration widget, we cannot use it separately inside the Container widget.
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child : Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1D1E33),     // as this is a default property of BoxDecoration widget, we cannot use it separately inside the Container widget.
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
